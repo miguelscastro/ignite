658b6d3bbc37ec147c963efcb5c8139c66d3ba31
miguelscastro <miguelscastro18@gmail.com>
2025-04-26 16:01:33 -0300

    chore: add company authentication and JWT

--- java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,16 +1,23 @@
 package br.com.miguelcastro.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.miguelcastro.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.miguelcastro.gestao_vagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     private final PasswordEncoder passwordEncoder;
     @Autowired
@@ -20,15 +27,22 @@ public class AuthCompanyUseCase {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public void execute(AuthCompanyDTO authCompanyDTO) {
+    public String execute(AuthCompanyDTO authCompanyDTO) {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
                 .orElseThrow(() -> {
-                    throw new UsernameNotFoundException("User not found");
+                    throw new UsernameNotFoundException("Usuário ou senha incorretos");
                 });
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
 
         if (!passwordMatches) {
             throw new BadCredentialsException("Usuário ou senha incorretos");
         }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
     }
 }
