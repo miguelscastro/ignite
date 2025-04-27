8649f5c55c4da6c6acbc6cf5a67946d3743b29af
miguelscastro <miguelscastro18@gmail.com>
2025-04-27 16:57:37 -0300

    chore: fix dependencies cycle through beans

--- java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/providers/JWTProvider.java
+++ java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/providers/JWTProvider.java
@@ -1,24 +1,22 @@
 package br.com.miguelcastro.gestao_vagas.providers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
-import br.com.miguelcastro.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
-
 @Service
 public class JWTProvider {
 
-    @Autowired
-    private AuthCompanyUseCase authCompanyUseCase;
+    @Value("${security.token.secret}")
+    public String secretKey;
 
     public String validateToken(String token) {
         token = token.replace("Bearer ", "");
 
-        Algorithm algorithm = Algorithm.HMAC256(authCompanyUseCase.secretKey);
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         try {
             var subject = JWT.require(algorithm)
