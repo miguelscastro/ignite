7f66b0a70439bad4f21f595f5a251ba9c076ff5d
miguelscastro <miguelscastro18@gmail.com>
2025-05-05 13:53:33 -0300

    chore: config PreAuthorize for candidate profile

--- java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/security/SecurityFilter.java
+++ java/gestao_vagas/src/main/java/br/com/miguelcastro/gestao_vagas/security/SecurityFilter.java
@@ -25,7 +25,6 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
-        SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if (request.getRequestURI().startsWith("/company")) {
