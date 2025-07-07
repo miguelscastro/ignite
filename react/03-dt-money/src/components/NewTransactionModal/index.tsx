f7cc849fcb0c308729edcfa27a063d9fcb5a7580
miguelscastro <miguelscastro18@gmail.com>
2025-07-07 17:17:26 -0300

    refactor: implement useContextSelector

--- react/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ react/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -14,8 +14,8 @@ import {
 import { z } from "zod/v4";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -27,8 +27,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext);
-
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction;
+    }
+  );
   const {
     control,
     register,
