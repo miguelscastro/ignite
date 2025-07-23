dfb9efc4c43df7c47caee371642d25bd84a5b061
miguelscastro <miguelscastro18@gmail.com>
2025-07-23 17:57:29 -0300

    feat: add dashboard page

--- react/04-pizza-shop-web/src/pages/app/dashboard/dashboard.tsx
+++ react/04-pizza-shop-web/src/pages/app/dashboard/dashboard.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet-async";
 
+import { PopularProductsChart } from "@/pages/popular-products-chart";
+
 import { DayOrdersAmountCard } from "./day-orders-amount-card";
 import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount-card";
 import { MonthOrdersAmountCard } from "./month-orders-amount-card";
@@ -22,6 +24,7 @@ export function Dashboard() {
 
         <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
