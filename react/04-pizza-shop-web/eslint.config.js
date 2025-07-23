b8fc16db7beadb843b87228979bfb6dd90cf5fa1
miguelscastro <miguelscastro18@gmail.com>
2025-07-23 12:25:05 -0300

    feat: add ThemeToggle and account menu

--- react/04-pizza-shop-web/eslint.config.js
+++ react/04-pizza-shop-web/eslint.config.js
@@ -42,7 +42,7 @@ export default [
           printWidth: 80,
           tabWidth: 2,
           singleQuote: false,
-          trailingComma: 'es5',
+          trailingComma: 'all',
           arrowParens: 'always',
           semi: true,
           endOfLine: 'auto',
