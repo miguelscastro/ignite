65fe2c111b09b84c6c68dffcd9bc3ee222849133
miguelscastro <miguelscastro18@gmail.com>
2025-07-25 13:08:31 -0300

    refactor: organize pizzashop depths and add api

---
+++ react/04-pizzashop/pizzashop-web/src/App.tsx
@@ -0,0 +1,20 @@
+import "./global.css";
+
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
+
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { routes } from "./routes";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </HelmetProvider>
+  );
+}
