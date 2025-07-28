f4b70b91848888647ea895b83cb0a9613716b7cb
miguelscastro <miguelscastro18@gmail.com>
2025-07-28 11:58:58 -0300

    feat: enable registration

--- react/04-pizzashop/pizzashop-web/src/pages/auth/sign-in.tsx
+++ react/04-pizzashop/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -2,7 +2,7 @@ import { Label } from "@radix-ui/react-label";
 import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 import { toast } from "sonner";
 import z4 from "zod/v4";
 
@@ -17,11 +17,17 @@ const signInFormSchema = z4.object({
 type SignInForm = z4.infer<typeof signInFormSchema>;
 
 export function SignIn() {
+  const [searchParams] = useSearchParams();
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>();
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get("email") ?? "",
+    },
+  });
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
