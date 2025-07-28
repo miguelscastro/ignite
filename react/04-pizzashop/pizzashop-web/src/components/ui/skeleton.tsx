fdc0d21e1ee5205ff3e637513d924adc3bdb9be9
miguelscastro <miguelscastro18@gmail.com>
2025-07-28 17:41:24 -0300

    fix: shadcn components broken due to dependencies version downgrade

--- react/04-pizzashop/pizzashop-web/src/components/ui/skeleton.tsx
+++ react/04-pizzashop/pizzashop-web/src/components/ui/skeleton.tsx
@@ -1,12 +1,14 @@
-import type React from "react";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      data-slot="skeleton"
-      className={cn("animate-pulse rounded-md bg-accent", className)}
+      className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
   );
