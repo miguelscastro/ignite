b42495231abb075eab7446873f113100cb98bdf6
miguelscastro <miguelscastro18@gmail.com>
2025-08-26 18:13:42 -0300

    chore: setup test environment

--- react/04-pizzashop/pizzashop-web/vite.config.ts
+++ react/04-pizzashop/pizzashop-web/vite.config.ts
@@ -2,9 +2,9 @@ import path from 'node:path'
 
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
+import { type InlineConfig } from 'vitest/node'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,4 +12,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
