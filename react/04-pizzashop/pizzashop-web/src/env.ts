5c9acbd6d9740ac20e175fd897a4f3ce662b8597
miguelscastro <miguelscastro18@gmail.com>
2025-09-11 15:01:18 -0300

    test(sign-in): authenticate user and set cookie with jwt

--- react/04-pizzashop/pizzashop-web/src/env.ts
+++ react/04-pizzashop/pizzashop-web/src/env.ts
@@ -2,7 +2,7 @@ import z4 from 'zod/v4'
 
 const envSchema = z4.object({
   MODE: z4.enum(['production', 'development', 'test']),
-  VITE_API_URL: z4.url(),
+  VITE_API_URL: z4.string(),
   VITE_ENABLE_API_DELAY: z4.string().transform((value) => value === 'true'),
 })
 
