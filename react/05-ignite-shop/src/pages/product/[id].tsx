d6fe1c006960cb1590d35f581ddc7591737052fd
miguelscastro <miguelscastro18@gmail.com>
2026-01-22 11:30:26 -0300

    wip: style product page

--- react/05-ignite-shop/src/pages/product/[id].tsx
+++ react/05-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,28 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer />
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          minus et maiores possimus omnis eligendi deserunt perspiciatis
+          distinctio, ea quisquam molestiae amet nobis sequi rerum magni
+          sapiente quibusdam cumque consequuntur.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
