801b43ed24d271130b6d60f04e593338e5d90238
miguelscastro <miguelscastro18@gmail.com>
2026-03-03 16:36:49 -0300

    feat: user validations

--- go/05-gobid/internal/usecase/user/create_user.go
+++ go/05-gobid/internal/usecase/user/create_user.go
@@ -7,17 +7,21 @@ import (
 )
 
 type CreateUserReq struct {
-	UserName     string `json:"user_name"`
-	PasswordHash []byte `json:"password_hash"`
-	Email        string `json:"email"`
-	Bio          string `json:"bio"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Bio      string `json:"bio"`
 }
 
 func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
 	var eval validator.Evaluator
+	eval.CheckField(validator.NotBlank(req.UserName), "user_name", "This field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Email), "email", "This field cannot be empty")
+	eval.CheckField(validator.Matches(req.Email, validator.EmailRX), "email", "must be a valid email")
+	eval.CheckField(validator.NotBlank(req.Bio), "bio", "This field cannot be empty")
 	eval.CheckField(
-		validator.NotBlank(req.UserName),
-		"user_name",
-		"This field cannot be empty")
+		validator.MinChars(req.Bio, 10) &&
+			validator.MaxChars(req.Bio, 255), "bio", "this field must have a length between 10 and 255")
+	eval.CheckField(validator.MinChars(req.Password, 8), "password", "password must be bigger than 8 characters")
 	return eval
 }
