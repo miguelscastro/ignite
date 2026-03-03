a9aac59b4005371bfece04a3444c553478f26e10
miguelscastro <miguelscastro18@gmail.com>
2026-03-03 14:19:34 -0300

    chore: prep environment and setup tern

---
+++ go/05-gobid/internal/api/api.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Api struct {
+	Router *chi.Mux
+}
+
+func (api *Api) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+}
